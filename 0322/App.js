7ac611cb80d437eba5f613a3168a95a93b43b7b7
Seoki55 <162667809+Seoki55@users.noreply.github.com>
2024-04-09 18:51:52 +0900

    Add files via upload

---
+++ 0322/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://Seoki55.github.io/webP1/0323/build/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          이재석, 폼 미쳤다!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
